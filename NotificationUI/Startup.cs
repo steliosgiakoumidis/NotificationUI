6575bd5abfa85413343fa7d002feb5668cc2dd55
Unknown <stelios.gia.sweden@gmail.com>
2019-04-20 23:13:25 +0200

    Priority turned into an enum

--- NotificationUI/Startup.cs
+++ NotificationUI/Startup.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
-using Serilog.Settings.Configuration;
 
 namespace NotificationUI
 {
